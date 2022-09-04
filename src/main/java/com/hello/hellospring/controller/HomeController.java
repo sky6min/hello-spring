adea307766d8918222aaf360794bd5a7d7dfc421
김창민 Kim Chang Min <51622501+sky6min@users.noreply.github.com>
2022-09-05 00:20:48 +0900

    Update HomeController.java

--- src/main/java/com/hello/hellospring/controller/HomeController.java
+++ src/main/java/com/hello/hellospring/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        System.out.println("sss);
+        System.out.println("sss");
         return "home";
     }
 }
